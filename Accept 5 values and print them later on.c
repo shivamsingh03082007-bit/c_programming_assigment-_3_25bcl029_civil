97e420baa1249f2e5d49f9bb827988fe100ae20f
shivamsingh.github19 <shivamsingh03082007@gmail.com>
2025-10-05 10:22:55 +0530

    Create Accept 5 values and print them later on.c

---
+++ Accept 5 values and print them later on.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int arr[5];
+    printf("Enter 5 values:\n");
+    for(int i = 0; i < 5; i++)
+        scanf("%d", &arr[i]);
+
+    printf("You entered:\n");
+    for(int i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
