a67f7e761ea2c54f6abd33da05660f9203ce81c3
shivamsingh.github19 <shivamsingh03082007@gmail.com>
2025-10-06 08:53:15 +0530

    Create Convert a string to lowercase.c

---
+++ Convert a string to lowercase.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove trailing newline if present
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+
+    for(int i = 0; str[i]; i++)
+        str[i] = tolower((unsigned char) str[i]);
+
+    printf("Lowercase: %s\n", str);
+    return 0;
+}
