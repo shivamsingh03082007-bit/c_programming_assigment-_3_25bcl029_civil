8b7f8bc51d0b5582a864795ff77d87aac155db1e
shivamsingh.github19 <shivamsingh03082007@gmail.com>
2025-10-06 08:53:49 +0530

    Update Convert a string to lowercase.c

--- Convert a string to lowercase.c
+++ Convert a string to lowercase.c
@@ -7,7 +7,7 @@ int main() {
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
-    // Remove trailing newline if present
+
     size_t len = strlen(str);
     if (len > 0 && str[len - 1] == '\n') {
         str[len - 1] = '\0';
